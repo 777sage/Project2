b4039f95efa1bbf179cab37afc172765852eb88f
unknown <mitch.ryan.harrison@gmail.com>
2018-05-10 15:24:34 -0400

    first commit on new-mitch-branch

---
+++ Project2/src/main/java/com/revature/selenium/LocationsPageStepDef.java
@@ -0,0 +1,83 @@
+package com.revature.selenium;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LocationsPageStepDef {
+	
+	private WebDriver driver;
+
+	public void initializeWebDriver(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	@When("^Locations tab clicked$")
+	public void locations_tab_clicked() throws Throwable {
+		System.out.println("Inside click locations");
+		driver.findElement(By.className("_md-nav-button-text ng-scope")).click();
+	}
+
+	@Then("^check if the Locations page is open$")
+	public void check_if_the_Locations_page_is_open() throws Throwable {
+		System.out.println("inside then check locations");
+	}
+
+	@Given("^a location$")
+	public void a_location() throws Throwable {
+		System.out.println("I am running in given location");
+	}
+
+	@Given("^with a building$")
+	public void with_a_building() throws Throwable {
+		System.out.println("inside with a buidling given");
+	}
+
+	@Given("^room$")
+	public void room() throws Throwable {
+		System.out.println("Inside room option");
+	}
+
+	@When("^the location is clicked$")
+	public void the_location_is_clicked() throws Throwable {
+		System.out.println("inside the location is clicked");
+	}
+
+	@Then("^does a building dropdown window open$")
+	public void does_a_building_dropdown_window_open() throws Throwable {
+		System.out.println("inside buidlign dropdown window open");
+	}
+
+	@When("^the building is clicked$")
+	public void the_building_is_clicked() throws Throwable {
+		System.out.println("Inside building is clicked");
+	}
+
+	@Then("^does a room dropdown window open$")
+	public void does_a_room_dropdown_window_open() throws Throwable {
+		System.out.println("inside does a room dropdown window open");
+	}
+
+	@When("^logout button clicked$")
+	public void logout_button_clicked() throws Throwable {
+		System.out.println("Inside logout button clicked");
+	}
+	
+	@Then("^check if the login screen is loaded$")
+	public void check_if_the_login_screen_is_loaded() throws Throwable {
+		System.out.println("inside check if the login screen is loaded");
+	}
+
+	@Then("^close the test$")
+	public void close_the_test() throws Throwable {
+		System.out.println("inside close the test");
+	}
+}
---
+++ Project2/src/test/java/com/revature/testng/LocationsTestRunner.java
@@ -0,0 +1,80 @@
+package com.revature.testng;
+
+import org.testng.annotations.Test;
+
+import com.revature.selenium.LoginPage;
+
+import cucumber.api.CucumberOptions;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+import cucumber.api.testng.*;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+@CucumberOptions(features="src/test/resources", glue="LocationsPageStepsDef")
+public class LocationsTestRunner extends AbstractTestNGCucumberTests {
+	
+//	@Test(dataProvider = "dp")
+//	public void locationsTest(Integer n, String s) {
+//	  
+//	}
+//	@BeforeMethod
+//	public void beforeMethod() {
+//	}
+
+
+//  @DataProvider
+//  public Object[][] dp() {
+//    return new Object[][] {
+//      new Object[] { 1, "a" },
+//      new Object[] { 2, "b" },
+//    };
+//  }
+//  @BeforeClass
+//  public void beforeClass() {
+//  }
+//
+//  @AfterClass
+//  public void afterClass() {
+//	  
+//  }
+//
+//  @BeforeTest
+//  public void beforeTest() {
+//	  File chrome = new File("src/test/resources/chromedriver.exe");
+//		System.setProperty("webdriver.chrome.driver", chrome.getAbsolutePath());
+//		driver = new ChromeDriver();
+//		driver.get("https://dev.assignforce.revaturelabs.com");
+//		try {
+//			  Properties props = new Properties();
+//			  InputStream in = new FileInputStream("src/main/resources/util.properties");
+//			  props.load(in);
+//			  String trainerUsername = props.getProperty("trainerUsername");
+//			  String trainerPassword = props.getProperty("trainerPassword");
+//			  driver.findElement(By.id("username")).sendKeys(trainerUsername);
+//			  driver.findElement(By.name("pw")).sendKeys(trainerPassword);
+//			  driver.findElement(By.xpath("//*[@id='Login']")).click();
+//		  } catch(Exception e) {
+//			  System.out.println(e.getMessage());
+//		  }
+//  }
+//
+//  @AfterTest
+//  public void afterTest() {  
+//	  driver.close();
+//	  driver.quit();
+//  }
+
+}
