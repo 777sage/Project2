b86b7b1f32be2c858d11ba788f8bc11b18f986fe
Andrew McGovern <winedark.mcgovern@gmail.com>
2018-05-10 13:29:37 -0400

    sent testng data to db

---
+++ Project2/src/main/java/com/revature/dao/TestDaoImpl.java
@@ -0,0 +1,103 @@
+package com.revature.dao;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import com.revature.model.Test;
+import com.revature.util.HibernateUtil;
+
+public class TestDaoImpl implements TestDao {
+
+	public boolean insertTest(Test tst) {
+		Session session = HibernateUtil.getSession().openSession();
+		Transaction t1 = null;
+		try {
+			t1 = session.beginTransaction();
+			session.save(tst);
+			t1.commit();
+			return true;
+
+		} catch (HibernateException h) {
+			if (t1 != null) {
+				t1.rollback();
+			}
+		} finally {
+			session.close();
+		}
+		return false;
+	}
+
+	public boolean updateTest(String name, String status) {
+		Session session = HibernateUtil.getSession().openSession();
+		Transaction t1 = null;
+		Test tst;
+		try {
+			t1 = session.beginTransaction();
+			Query query = session.createQuery("FROM Test WHERE testName=:name");
+			query.setParameter("name", name);
+			tst = (Test) query.getSingleResult();
+			tst.setStatus(status);
+			Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+			tst.setTimestamp(timestamp);
+			session.saveOrUpdate(tst);
+			t1.commit();
+			return true;
+		} catch(NoResultException n) {
+			tst=new Test(name,status);
+			session.save(tst);
+			t1.commit();
+			return true;
+		}
+		catch (HibernateException h) {
+			if (t1 != null) {
+				t1.rollback();
+			}
+		} finally {
+			session.close();
+		}
+		return false;
+	}
+
+	public Test findTestByName(String name) {
+		Session session = HibernateUtil.getSession().openSession();
+		Test tst = null;
+		try {
+			session.beginTransaction();
+			Query query = session.createQuery("FROM Test WHERE testName=:name");
+			query.setParameter("name", name);
+			tst = (Test) query.getSingleResult();
+		} catch (HibernateException h) {
+			//
+		} finally {
+			session.close();
+		}
+		return tst;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Test> getAllTests() {
+		Session session = HibernateUtil.getSession().openSession();
+		List<Test> Tests = null;
+		try {
+			session.beginTransaction();
+			Tests = session.createQuery("from com.revature.model.Test").list();
+		} catch (HibernateException h) {
+			//
+		} finally {
+			session.close();
+		}
+		return Tests;
+	}
+
+	public boolean deleteTest(int id) {
+		return false;
+		// TODO Auto-generated method stub
+
+	}
+}
--- Project2/src/main/java/com/revature/selenium/LoginPage.java
+++ Project2/src/main/java/com/revature/selenium/LoginPage.java
@@ -7,11 +7,14 @@ import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginPage {
 	String trainerUsername;
 	String trainerPassword;	
+	String vpUsername;
+	String vpPassword;
 	
 	public static void main(String[] args) {
 //		File chrome = new File("src/main/resources/chromedriver");
@@ -31,6 +34,8 @@ public class LoginPage {
 		  props.load(in);
 		  trainerUsername = props.getProperty("trainerUsername");
 		  trainerPassword = props.getProperty("trainerPassword");
+		  vpUsername = props.getProperty("vpUsername");
+		  vpPassword = props.getProperty("vpPassword");
 	  } catch(Exception e) {
 		  System.out.println(e.getMessage());
 	  }
@@ -44,19 +49,39 @@ public class LoginPage {
 		return driver.getTitle();
 	}
 	
-	public void loginAsTrainer(WebDriver driver) {
-		findLoginPage(driver);
-		driver.findElement(By.id("username")).sendKeys(trainerUsername);
-		driver.findElement(By.name("pw")).sendKeys(trainerPassword);
-		driver.findElement(By.xpath("//*[@id='Login']")).click();
-	}
-	
 	public void loginAsTrainerWithBadCredential(WebDriver driver) {
 		findLoginPage(driver);
 		driver.findElement(By.id("username")).sendKeys("Some Gibberish");
 		driver.findElement(By.name("pw")).sendKeys("Some Gibberish");
 		driver.findElement(By.xpath("//*[@id='Login']")).click();
+		WebElement error = driver.findElement(By.id("error"));
+		
 	}
+	
+	public void loginAsTrainer(WebDriver driver) {
+		findLoginPage(driver);
+		driver.findElement(By.id("username")).sendKeys(trainerUsername);
+		driver.findElement(By.name("pw")).sendKeys(trainerPassword);
+		driver.findElement(By.xpath("//*[@id='Login']")).click();
+		
+		if (driver.getTitle().equals("Verify Your Identity | Salesforce")) {
+			driver.findElement(By.id("emc")).sendKeys("49618");
+			driver.findElement(By.id("save")).click();
+		}
+	}
+	
+	public void loginAsVp(WebDriver driver) {
+		findLoginPage(driver);
+		driver.findElement(By.id("username")).sendKeys(trainerUsername);
+		driver.findElement(By.name("pw")).sendKeys(trainerPassword);
+		driver.findElement(By.xpath("//*[@id='Login']")).click();
+		
+		if (driver.getTitle().equals("Verify Your Identity | Salesforce")) {
+			driver.findElement(By.id("emc")).sendKeys("49618");
+			driver.findElement(By.id("save")).click();
+		}
+	}
+	
 	
 
 }
